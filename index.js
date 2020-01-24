fbfc899d2cdb82133d478ebdf732902d00525ca5
Sam Orgill <ssorgill@icloud.com>
2020-01-24 16:13:09 +0000

    Sync chains

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
@@ -7,6 +8,8 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({blockchain});
 
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
@@ -24,7 +27,17 @@ app.post(`/${api}/mine`, (req, res) => {
     res.redirect(`/${api}/blocks`);
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+    request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+
+            console.log('replace chain on a sync with ', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    })
+};
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
@@ -32,4 +45,7 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
 }
 
 const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, () => console.log(`App listening on localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`App listening on localhost:${PORT}`);
+    syncChains();
+});
