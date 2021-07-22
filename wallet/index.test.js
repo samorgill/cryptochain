6a229569c9d8baad8e039b7592f060b5a5816d01
Sam Orgill <ssorgill@icloud.com>
2021-07-22 08:14:42 +0100

    Update transactions with recipient amount

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index');
+const Transaction = require('./transaction');
 const {verifySignature} = require('../util');
 
 describe('Wallet', () => {
@@ -40,6 +41,38 @@ describe('Wallet', () => {
         });
 
     });
+
+    describe('createTransaction()', () => {
+        describe('and the amount exceeds the balance', () => {
+            it('throws an error ', () => {
+                expect(() => wallet.createTransaction({amount: 999999, recipient: 'foo-recipient'}))
+                    .toThrow('Amount exceeds balance');
+            });
+        });
+
+        describe('and the amount is valid', () => {
+
+            let transaction, amount, recipient;
+
+            beforeEach(() => {
+                amount = 50;
+                recipient = 'foo-recipient';
+                transaction = wallet.createTransaction({amount, recipient})
+            })
+
+            it('creates and instance of `Transaction`', () => {
+                expect(transaction instanceof Transaction).toBe(true);
+            })
+
+            it('matches the transaction input with the wallet',  () => {
+                expect(transaction.input.address).toEqual(wallet.publicKey);
+            });
+
+            it('outputs the amount to the recipient ',  () => {
+                expect(transaction.outputMap[recipient]).toEqual(amount);
+            });
+        })
+    })
 
 });
 
